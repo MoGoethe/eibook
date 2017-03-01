3935f2070ddb1e381cdc8e78e42ba1ee0d1f333c
seaseeYoul <seaseeyoul@seaseeYouldeMacBook-Pro.local>
2017-03-01 22:59:30 +0800

    2017-03-01 23:00

--- routes/book.js
+++ routes/book.js
@@ -1,6 +1,12 @@
 var express = require('express');
 
 var router = express.Router();
+
+router.param('id',(req,res,next,id)=>{
+
+	next();
+})
+
 router.get('/book/:id', function(req, res, next) {
 	res.send('respond with a resource of book witch id is eql ID');
 });
