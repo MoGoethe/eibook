65dd952d0b4343e025a25f70aa88b97d77906fbd
seaseeYoul <seaseeyoul@seaseeYouldeMacBook-Pro.local>
2017-03-04 23:02:05 +0800

    2017-03-03 23:01

--- routes/notifications.js
+++ routes/notifications.js
@@ -1,40 +1,81 @@
 const express = require('express');
 const router = express.Router();
 
-router.param('name',(req,res,next,name)=>{
 /*
-name:
-	new
-	weekly
-	recommend
-	history
-	random
-对分支进行解析，获取数据，传递数据。 ejs负责渲染
+	####消息页面
+	* /notifications/comments	评论提醒（√）
+	* /notifications/chats		交谈对话（√）
+	* /notifications/requests		请求（？是否需要）（×）
+	* /notifications/likes		点赞提醒（√）
+	* /notifications/follow		关注提醒（√）
+	* /notifications/other		其他消息提醒（×）
 */	
-	switch(name){
-		case 'new':
-			console.log('new');
+router.param('moduleName',(req,res,next,moduleName)=>{
+	res.locals.displayBlock = {
+		comments:false, 
+		messages:false,
+		request:false,
+		follow:false,
+		shelve:false,
+		likes:false,
+		others:false
+	}
+	switch(moduleName){
+		case 'comments':
+			res.locals.displayBlock[moduleName] = true;
 			break;
-		case 'weekly':
-			console.log('weekly');
+		case 'messages':
+			res.locals.displayBlock[moduleName] = true;
 			break;
-		case 'recommend':
-			console.log('recommend');
+		case 'request':
+			res.locals.displayBlock[moduleName] = true;
 			break;
-		case 'history':
-			console.log('history');
+		case 'follow':
+			res.locals.displayBlock[moduleName] = true;
 			break;
-		case 'random':
-			console.log('random');
+		case 'shelve':
+			res.locals.displayBlock[moduleName] = true;
+			break;
+		case 'likes':
+			res.locals.displayBlock[moduleName] = true;
+			break;
+		case 'others':
+			res.locals.displayBlock[moduleName] = true;
 			break;
 		default:
-			res.redirect('/404');
+			res.redirect("/404");
+			break;
 	}
 	next();
 })
 
-router.get('/notifications/:name', function(req, res, next) {
-        res.render('notifications',{name:'notifications  '+req.params.name});
+function data(req,res,next){
+	res.locals.data = {
+		login_status : false,
+		user_id: false,
+		displayBlock:{}
+	};
+	if(typeof req.session.__LOGINSTATUS !== 'undefined' && typeof req.session.__USERID !== 'undefined'){
+		res.locals.data.login_status = req.session.__LOGINSTATUS;
+		res.locals.data.user_id = req.session.__USERID;
+	}
+	res.locals.data['displayBlock'] = res.locals.displayBlock;
+	next();
+}
+
+
+router.get('/notifications/:moduleName', data,function(req, res, next) {
+        res.render('notifications');
 });
 
 module.exports = router;
+
+
+
+
+
+
+
+
+
+
